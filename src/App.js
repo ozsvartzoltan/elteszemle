cdb1f82ce7c709b1c16497ed3f45020798ca729c
Zoltan Ozsvart <ozsivarga@hotmail.com>
2025-01-22 18:59:11 +0100

    no google sheets

--- src/App.js
+++ src/App.js
@@ -3,29 +3,9 @@ import { useEffect, useState } from "react";
 function App() {
   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("/api/get-data"); // Backend API endpoint
-        const result = await response.json();
-        setData(result.data);
-        console.log(result.data);
-      } catch (error) {
-        console.error("Hiba történt az adatok lekérésekor:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <div>
-      <h1>Google Sheets Adatok</h1>
-      <ul>
-        {data.map((row, index) => (
-          <li key={index}>{row.join(", ")}</li>
-        ))}
-      </ul>
+      <h1>Hamarosan érkezünk...</h1>
     </div>
   );
 }
