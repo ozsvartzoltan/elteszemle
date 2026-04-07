e64f323d747649169b1fb66f97d7715a076a3ced
Zoltan Ozsvart <75853960+ozsvartzoltan@users.noreply.github.com>
2026-04-07 12:54:39 +0200

    for build

--- src/contexts/DataContext.js
+++ src/contexts/DataContext.js
@@ -6,7 +6,7 @@ const DataContext = createContext()
 export const useData = () => {
   const context = useContext(DataContext)
   if (!context) {
-    throw new Error('useData must be used within a DataProvider')
+    throw new Error('useData must be used within a DataProvider.')
   }
   return context
 }
